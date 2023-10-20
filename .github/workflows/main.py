7a83f07b8150d0a35f78073d61d7e6a42ebc4517
vettarine <0nezenka0@mail.ru>
2023-10-20 17:21:36 +0300

    commit2

--- .github/workflows/main.py
+++ .github/workflows/main.py
@@ -1,6 +1,23 @@
 # coctail sort
 
-def CoctailSort(array):
-    print("send nudes")
-    return 0
+def CoctailSort(nums):
+    for i in range(len(nums) - 1, 0, -1):
+        is_swapped = False
+
+        for j in range(i, 0, -1):
+            if nums[j] < nums[j - 1]:
+                nums[j], nums[j - 1] = nums[j - 1], nums[j]
+                is_swapped = True
+
+        for j in range(i):
+            if nums[j] > nums[j + 1]:
+                nums[j], nums[j + 1] = nums[j + 1], nums[j]
+                is_swapped = True
+
+        if not is_swapped:
+            return nums
+
+
+nums = [15, 37, 69, 26, 78]
+print(CoctailSort(nums))
 
