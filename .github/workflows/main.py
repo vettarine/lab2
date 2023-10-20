022798c730c2ddc4cd163b8fe5d219fe75505fd0
vettarine <0nezenka0@mail.ru>
2023-10-20 17:51:19 +0300

    commit4

--- .github/workflows/main.py
+++ .github/workflows/main.py
@@ -1,23 +1,23 @@
 # cocktail sort
 
-def cocktail_sort(nums):
-    for i in range(len(nums) - 1, 0, -1):
+def cocktail_sort(array):
+    for i in range(len(array) - 1, 0, -1):
         is_swapped = False
 
         for j in range(i, 0, -1):
-            if nums[j] < nums[j - 1]:
-                nums[j], nums[j - 1] = nums[j - 1], nums[j]
+            if array[j] < array[j - 1]:
+                array[j], array[j - 1] = array[j - 1], array[j]
                 is_swapped = True
 
         for j in range(i):
-            if nums[j] > nums[j + 1]:
-                nums[j], nums[j + 1] = nums[j + 1], nums[j]
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
                 is_swapped = True
 
         if not is_swapped:
-            return nums
+            return array
 
 
-nums = [15, 37, 69, 26, 78]
-print(cocktail_sort(nums))
+array = [15, 37, 69, 26, 78]
+print(cocktail_sort(array))
 print('yazaebalas')
