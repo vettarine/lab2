7c86f49eae3c226f6d92809de94b57c0168dea05
vettarine <0nezenka0@mail.ru>
2023-11-16 21:33:31 +0300

    добавление выделенных модулей в main

--- .github/workflows/main.py
+++ .github/workflows/main.py
@@ -1,11 +1,10 @@
 # cocktail sort
 
-from sorting import cocktail_sort
 from array_generating import array_generating
+from printing import print_array, print_salute
 
 
+print_salute()
 num = int(input("Введите количество чисел для сортировки: "))
 array = array_generating(num)
-
-
-
+print_array(array)
