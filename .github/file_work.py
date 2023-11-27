90109c64e7f31fa8c121ad019760355a87d238de
vettarine <0nezenka0@mail.ru>
2023-11-27 12:12:22 +0300

    сохранение в файл

---
+++ .github/file_work.py
@@ -0,0 +1,3 @@
+def file_save(array):
+    with open('file.txt', 'w') as f:
+        f.write(str(array))
--- .github/main.py
+++ .github/main.py
@@ -3,10 +3,13 @@
 from array_generating import array_generating
 from printing import print_array, print_salute
 from sorting import cocktail_sort
+from file_work import file_save
 
 
 print_salute()
 num = int(input("Введите количество чисел для сортировки: "))
 array = array_generating(num)
+sorted_array = cocktail_sort(array)
 print_array(array, "Оригинальный массив: ")
-print_array(cocktail_sort(array), "Отсортированный массив: ")
+print_array(sorted_array, "Отсортированный массив: ")
+file_save(sorted_array)
