7a2f212fe01e957f989419a7ceeac5fc3887ce4d
vettarine <0nezenka0@mail.ru>
2023-11-27 12:07:29 +0300

    фикс модуля принтинга

--- .github/main.py
+++ .github/main.py
@@ -2,9 +2,11 @@
 
 from array_generating import array_generating
 from printing import print_array, print_salute
+from sorting import cocktail_sort
 
 
 print_salute()
 num = int(input("Введите количество чисел для сортировки: "))
 array = array_generating(num)
-print_array(array)
+print_array(array, "Оригинальный массив: ")
+print_array(cocktail_sort(array), "Отсортированный массив: ")
--- .github/printing.py
+++ .github/printing.py
@@ -1,12 +1,10 @@
-from sorting import cocktail_sort
-
-
 def print_salute():
     """Print the salute"""
     print("Шейкерная сортировка. Вариант 1.")
     print("Работу выполнили студенты 424 группы: Бертова Альбина, Миронова Таисия, Середенко Ульяна")
 
-def print_array(array):
+def print_array(array, text):
     """Print original and sorted array"""
-    print("Оригинальный массив: ", array)
-    print("Отсортированный массив: ", cocktail_sort(array))
+    print(text, array)
+
+
